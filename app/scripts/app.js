4cf4999005303c7595db1dc32707fd077a67ba61
James Hines <jameshines10@gmail.com>
2015-06-29 21:00:10 -0400

    Adds User, TravelService and WeatherService services. Adds a simple test of the User and TravelService services in the run block

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,12 +11,56 @@
 angular
   .module('angularPromisesApp', [])
 
-  .run(function(SimpleService){
-    SimpleService.saySomething();
+  .run(function(User, TravelService, WeatherService){
+    TravelService
+      .getDeparture(User.email)
+      .then(function(departure){
+        console.log(departure);
+      });
   })
 
-  .service('SimpleService', function(){
-    this.saySomething = function(){
-      console.log('Hello World!');
+  .factory('User', function(){
+    return {
+      name: 'James Hines',
+      email: 'jameshines10@gmail.com'
+    }
+  })
+
+  .factory('TravelService', function($q, User){
+    return {
+      getDeparture: function(){
+        var deferred = $q.defer();
+
+        deferred.resolve({
+          userID: User.email,
+          flightID: 'US_343223',
+          date: '06/29/2015 8:00AM'
+        });
+
+        return deferred.promise;
+      },
+
+      getFlight: function(flightID) {
+        return $q.resolve({
+          id: flightID,
+          pilot: 'Captain Morgan',
+          plane: {
+            make: 'Boeing 747 RC',
+            model: 'TA-889'
+          },
+          status: 'onTime'
+        });
+      }
+    }
+  })
+
+  .factory('WeatherService', function($q) {
+    return {
+      getForecast: function(date) {
+        return $q.resolve({
+          date: date,
+          forecast: 'rain'
+        });
+      }
     }
   });
